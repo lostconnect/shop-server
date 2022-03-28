5def043aece2c756ff0801bbd21a7522981ade62
dmitry-kungurov <dmitry-kungurov@idpowers.com>
2022-03-28 18:01:10 +0400

    create jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,62 @@
 //подключим
+const User = require("../models/user");
 const ApiError = require("../error/ApiError");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign(
+    {
+      id,
+      email,
+      role,
+    },
+    process.env.SECRET_KEY
+  );
+};
 
 class UserController {
-  async registration(req, res) {}
-
-  async login(req, res) {}
-  async check(req, res, next) {
-    const { id } = req.query;
-
-    //проверка
-    if (!id) {
-      return next(ApiError.badRequest("Не задан id"));
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest("не указан email или password"));
     }
 
-    res.json({ id });
+    const condidate = await User.findOne({ email });
+    if (condidate) {
+      return next(ApiError.badRequest("Пользователь уже существует"));
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({
+      email,
+      password: hashPassword,
+      role,
+    });
+
+    const token = generateJwt(user.id, user.email, user.role);
+
+    return res.json({ token });
+  }
+
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(ApiError.internal("Пользователь не найден"));
+    }
+
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("Неверный пароль"));
+    }
+
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
+  async check(req, res, next) {
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
   }
 }
 
--- routes/productRuoter.js
+++ routes/productRuoter.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const router = Router();
 const ProductController = require("../controllers/productController");
+const checkRole = require("../middleware/checkRoleMiddleware");
 
-router.post("/create", ProductController.create);
+router.post("/create", checkRole("ADMIN"), ProductController.create);
 router.get("/all", ProductController.getAll);
 router.get("/:id", ProductController.getOne);
 router.get("/delete/:id", ProductController.delete);
